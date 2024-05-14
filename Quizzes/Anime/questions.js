f420e31bc0420d800034217df6b726133a903dd4
KingAJ19 <anelesokwane@gmail.com>
2024-05-14 19:12:08 +0200

    question

--- Quizzes/Anime/questions.js
+++ Quizzes/Anime/questions.js
@@ -264,14 +264,14 @@ let questions = [
     ]
   },
   {
-    numb: 4,
-    question: "What anime has a protagonist who aims to become the Pirate King?",
-    answer: "One Piece",
+    numb: 25,
+    question: "In ‘One Punch Man’, who is Saitama’s disciple?",
+    answer: "Genos",
     options: [
-      "One Piece",
-      "Sol Bianca: The Legacy",
-      "Queen Emeraldas",
-      "Bodacious Space Pirates"
+      "Metal Bat",
+      "Zombieman",
+      "King",
+      "Genos"
     ]
   },
   {
