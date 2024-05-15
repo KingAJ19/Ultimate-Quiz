afa278fbe229346a8efc177fc9b12479d0fbb076
KingAJ19 <anelesokwane@gmail.com>
2024-05-15 13:37:11 +0200

    linked the files

---
+++ Quizzes/Basketball/questions.js
@@ -0,0 +1,554 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the highest grossing anime film of all time?",
+    answer: "Mugen Train",
+    options: [
+      "Grave Of The Fireflies",
+      "Your Name",
+      "Mugen Train",
+      "Dragon Ball Super: Broly"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the first anime ever produced?",
+    answer: "Nakamura Gatana",
+    options: [
+      "Nakamura Gatana",
+      "Sailor Moon",
+      "Neon Genesis Evangelion",
+      "Rurouni Kenshin"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What popular anime features a notebook that can kill people by writing their names in it?",
+    answer: "Death Note",
+    options: [
+      "Love Tyrant",
+      "Death Parade",
+      "Death Note",
+      "FullMetal Alchemist"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What anime has a protagonist who aims to become the Pirate King?",
+    answer: "One Piece",
+    options: [
+      "One Piece",
+      "Sol Bianca: The Legacy",
+      "Queen Emeraldas",
+      "Bodacious Space Pirates"
+    ]
+  },
+  {
+    numb: 5,
+    question: "In Naruto, what is the highest rank a ninja can achieve?",
+    answer: "Hokage",
+    options: [
+      "Chūnin",
+      "Hokage",
+      "Jōnin",
+      "Genin"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the name of the giant humanoid creatures in ‘Attack on Titan’?",
+    answer: "Titans",
+    options: [
+      "Titans",
+      "Monsters",
+      "Demons",
+      "Colossus"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the name of the virtual world in ‘Sword Art Online’?",
+    answer: "Aincrad",
+    options: [
+      "Underworld",
+      "Gun Gale",
+      "Alfheim",
+      "Aincrad"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who is the highest-ranking hero in ‘One Punch Man’?",
+    answer: "Blast",
+    options: [
+      "Blast",
+      "Tornado of Terror",
+      "Drive Knight",
+      "Silver Fang"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the unique power system in ‘Hunter x Hunter’ called?",
+    answer: "Nen",
+    options: [
+      "Cursed Energy",
+      "Ki",
+      "Nen",
+      "Chakra"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the name of the currency in ‘One Piece’?",
+    answer: "Belly",
+    options: [
+      "Belly",
+      "Zeni",
+      "Double Dollar",
+      "Woolong"
+    ]
+  },
+  {
+    numb: 11,
+    question: "What’s the name of Goku’s wife in the ‘Dragon Ball’ series?",
+    answer: "Chi-Chi",
+    options: [
+      "Mai",
+      "Bulma",
+      "Chi-Chi",
+      "Caulifla"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What type of food does Naruto Uzumaki love the most?",
+    answer: "Ramen",
+    options: [
+      "Dango",
+      "Ramen",
+      "Curry Of Life",
+      "Onigiri"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Who is the ‘Fullmetal Alchemist’?",
+    answer: "Edward Elric",
+    options: [
+      "Alphonse Elric",
+      "Ling Yao",
+      "Roy Mustang",
+      "Edward Elric"
+    ]
+  },
+  {
+    numb: 14,
+    question: "In ‘Neon Genesis Evangelion’, what are the giant bio-machines piloted by the main characters called?",
+    answer: "Evangelions",
+    options: [
+      "Evangelions",
+      "Angels",
+      "Gurren Lagann",
+      "Getter Robo"
+    ]
+  },
+  {
+    numb: 15,
+    question: "What is the main sport featured in ‘Haikyuu!!’?",
+    answer: "Volleyball",
+    options: [
+      "Tennis",
+      "Soccer",
+      "Basketball",
+      "Volleyball"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Who is the founder and first Hokage of the Hidden Leaf Village in ‘Naruto’?",
+    answer: "Hashirama Senju",
+    options: [
+      "Tobirama Senju",
+      "Hashirama Senju",
+      "Hiruzen Sarutobi",
+      "Tsunade"
+    ]
+  },
+  {
+    numb: 17,
+    question: "In ‘My Hero Academia’, what is All Might’s quirk?",
+    answer: "One For All",
+    options: [
+      "All For One",
+      "Whirlwind",
+      "One For All",
+      "Shield"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Who is the original creator of the ‘One Piece’ manga?",
+    answer: "Eiichiro Oda",
+    options: [
+      "Eiichiro Oda",
+      "Tatsuki Fujimoto",
+      "Akira Toriyama",
+      "Makoto Yukimura"
+    ]
+  },
+  {
+    numb: 19,
+    question: "In ‘JoJo’s Bizarre Adventure’, what are the supernatural abilities used by characters known as?",
+    answer: "Stands",
+    options: [
+      "Geass",
+      "Rinnegan",
+      "Ultra Instinct",
+      "Stands"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Who is the protagonist of ‘Fairy Tail’?",
+    answer: "Natsu Dragneel",
+    options: [
+      "Erza Scarlet",
+      "Natsu Dragneel",
+      "Lucy Heartfilia",
+      "Grey Fullbuster"
+    ]
+  },
+  {
+    numb: 21,
+    question: "In ‘Tokyo Ghoul’, what do ghouls eat?",
+    answer: "Human flesh",
+    options: [
+      "Nothing",
+      "Burger King",
+      "Coffee",
+      "Human flesh"
+    ]
+  },
+  {
+    numb: 22,
+    question: "In ‘Death Note’, what is the name of the Shinigami who drops the Death Note?",
+    answer: "Ryuk",
+    options: [
+      "Ryuk",
+      "Midora",
+      "Rem",
+      "Sidoh"
+    ]
+  },
+  {
+    numb: 23,
+    question: "Who is the protagonist in ‘Inuyasha’?",
+    answer: "Inuyasha",
+    options: [
+      "Kagome",
+      "Shippo",
+      "Inuyasha",
+      "Kirara"
+    ]
+  },
+  {
+    numb: 24,
+    question: "In ‘Hunter x Hunter’, what is the name of Gon’s father?",
+    answer: "Ging Freecss",
+    options: [
+      "Isaac Netero",
+      "Ging Freecss",
+      "Pariston Hill",
+      "Gashta Bellam"
+    ]
+  },
+  {
+    numb: 25,
+    question: "In ‘One Punch Man’, who is Saitama’s disciple?",
+    answer: "Genos",
+    options: [
+      "Metal Bat",
+      "Zombieman",
+      "King",
+      "Genos"
+    ]
+  },
+  {
+    numb: 26,
+    question: "Who solved the Millennium Puzzle?",
+    answer: "Yugi Mutou",
+    options: [
+      "Seto Kaiba",
+      "Maximillion Pegasus",
+      "Yugi Mutou",
+      "Yami Bakura"
+    ]
+  },
+  {
+    numb: 27,
+    question: "Which anime series revolves around a boy who sells his soul to a demon?",
+    answer: "Black Butler",
+    options: [
+      "Naruto",
+      "Black Butler",
+      "Blue Exorcist",
+      "Claymore"
+    ]
+  },
+  {
+    numb: 28,
+    question: "Whose body gained the properties of rubber after unintentionally eating a Devil Fruit?",
+    answer: "Monkey D. Luffy",
+    options: [
+      "Monkey D. Luffy",
+      "Prince Grus",
+      "Charlotte Katakuri",
+      "Marco"
+    ]
+  },
+  {
+    numb: 29,
+    question: "In the anime ‘Claymore’, who was the most powerful of the ‘Awakened Beings’?",
+    answer: "Priscilla",
+    options: [
+      "Irene",
+      "Rosemary",
+      "Teresa",
+      "Priscilla"
+    ]
+  },
+  {
+    numb: 30,
+    question: "In ‘Clannad’, when Sanae first met Okazaki, which kind of bread does she offer to him?",
+    answer: "Sweet Bean Bread",
+    options: [
+      "Sweet Bean Bread",
+      "Red Bean Bun",
+      "Dango",
+      "Ramen"
+    ]
+  },
+  {
+    numb: 31,
+    question: "Which is the first song used as an opening for ‘Naruto’?",
+    answer: "‘Rocks’ by Hound Dog",
+    options: [
+      "‘Lovers’ by Seven Oops",
+      "‘Empty Hearts’ by Anly",
+      "‘Rocks’ by Hound Dog",
+      "‘Far Away’ by Asian Kung-Fu Generation"
+    ]
+  },
+  {
+    numb: 32,
+    question: "Who was the first member of the First Levi Squad to die in ‘Attack on Titan’?",
+    answer: "Gunther",
+    options: [
+      "Eld",
+      "Gunther",
+      "Petra",
+      "Connie"
+    ]
+  },
+  {
+    numb: 33,
+    question: "As prophesied, Goku transformed into the Super Saiyan form in ‘Dragon Ball Z’. Where did this transformation take place?",
+    answer: "Planet Namek",
+    options: [
+      "Eros",
+      "Planet Arlia",
+      "Planet Vegeta",
+      "Planet Namek"
+    ]
+  },
+  {
+    numb: 34,
+    question: "This pink-haired girl is completely obsessed with her love for Yuki, which she professes in her ‘Yukiteru diary’. Who is she?",
+    answer: "Yuno Gasai",
+    options: [
+      "Minene Uryuu",
+      "Tsubaki Kasugano",
+      "Yuno Gasai",
+      "Ai Mikami"
+    ]
+  },
+  {
+    numb: 35,
+    question: "Which anime series involves a family possessed by the animals of the Chinese zodiac?",
+    answer: "Fruits Basket",
+    options: [
+      "Fairy Tail",
+      "Sailor Moon",
+      "Jūni Taisen",
+      "Fruits Basket"
+    ]
+  },
+  {
+    numb: 36,
+    question: "What was the first feature-length anime?",
+    answer: "Momotaro: Sacred Sailors",
+    options: [
+      "Momotaro: Sacred Sailors",
+      "Horus: Prince of the Sun",
+      "Castle in the Sky",
+      "My Neighbor Totoro"
+    ]
+  },
+  {
+    numb: 37,
+    question: "Who is known as the ‘One Punch Man’?",
+    answer: "Saitama",
+    options: [
+      "King",
+      "Saitama",
+      "Bushidrill",
+      "Twin Tail"
+    ]
+  },
+  {
+    numb: 38,
+    question: "Which anime series centers around a warrior named Kenshiro?",
+    answer: "Fist of the North Star",
+    options: [
+      "Fist of the North Star", 
+      "Baki",
+      "Trigun",
+      "JoJo's Bizarre Adventure"
+    ]
+  },
+  {
+    numb: 39,
+    question: "What is the name of Speed Racer’s girlfriend?",
+    answer: "Trixie",
+    options: [
+      "Kurio", 
+      "Sparky",
+      "Junko",
+      "Trixie"
+    ]
+  },
+  {
+    numb: 40,
+    question: "Who was the first character in ‘Dragon Ball Z’ who achieved the Super Saiyan 2?",
+    answer: "Gohan",
+    options: [
+      "Goku", 
+      "Gohan",
+      "Vegeta",
+      "Trunks"
+    ]
+  },
+  {
+    numb: 41,
+    question: "What is the longest-running anime series?",
+    answer: "Sazae-san",
+    options: [
+      "Nintama Rantaro", 
+      "Ojarumaru",
+      "Oyako Club",
+      "Sazae-san"
+    ]
+  },
+  {
+    numb: 42,
+    question: "Which anime tells the tale of a heroic gunslinger with a $60 billion dollar bounty on his head?",
+    answer: "Trigun",
+    options: [
+      "Trigun", 
+      "One Piece",
+      "Cowboy Bebop",
+      "Angel Beats"
+    ]
+  },
+  {
+    numb: 43,
+    question: "Who is the creator of the popular anime and manga series 'Naruto'?",
+    answer: "Masashi Kishimoto",
+    options: [
+      "Masashi Kishimoto", 
+      "Naoki Urasawa",
+      "Akira Toriyama",
+      "Eiichiro Oda"
+    ]
+  },
+  {
+    numb: 44,
+    question: "What is the name of the magical girl protagonist in the anime series 'Sailor Moon'?",
+    answer: "Usagi Tsukino",
+    options: [
+      "Rei Hino", 
+      "Lilica Hubert",
+      "Usagi Tsukino",
+      "Hotaru Tomoe"
+    ]
+  },
+  {
+    numb: 45,
+    question: "In the anime series 'Bleach,' what is the name of the main character who becomes a Soul Reaper?",
+    answer: "Ichigo Kurosaki",
+    options: [
+      "Renji Abarai", 
+      "Rukia Kuchiki",
+      "Karin Kurosaki",
+      "Ichigo Kurosaki"
+    ]
+  },
+  {
+    numb: 46,
+    question: "Which anime series features a group of high school students forming a music band called 'Hokago Tea Time'?",
+    answer: "K-On!",
+    options: [
+      "Clannad", 
+      "Tamako Market",
+      "K-On!",
+      "Blue Period"
+    ]
+  },
+  {
+    numb: 47,
+    question: "What is the name of the navigator and thief who joins Luffy on his journey in the 'One Piece' manga series?",
+    answer: "Nami",
+    options: [
+      "Zoro", 
+      "Nami",
+      "Robin",
+      "Chopper"
+    ]
+  },
+  {
+    numb: 48,
+    question: "In what year was the 'Slam Dunk' manga first published?",
+    answer: "1990",
+    options: [
+      "1992", 
+      "1991",
+      "1989",
+      "1990"
+    ]
+  },
+  {
+    numb: 49,
+    question: "Who is Naruto's teacher in the 'Naruto' manga series?",
+    answer: "Iruka",
+    options: [
+      "Hiruzen", 
+      "Asuma",
+      "Iruka",
+      "Tsunade"
+    ]
+  },
+  {
+    numb: 50,
+    question: "At the conclusion of the 'Naruto' manga series, who does Naruto marry?",
+    answer: "Hinata",
+    options: [
+      "Hinata", 
+      "Sakura",
+      "Ino",
+      "Tenten"
+    ]
+  },
+    
+];
+
