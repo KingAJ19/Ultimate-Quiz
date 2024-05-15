31cc3f61631cebd7304803eb9fdfec65677650c3
KingAJ19 <anelesokwane@gmail.com>
2024-05-15 20:24:11 +0200

    added the stuff

---
+++ Quizzes/Football/questions.js
@@ -0,0 +1,554 @@
+let questions = [
+    {
+    numb: 1,
+    question: "George Gervin’s 'most points scored in a quarter’ record was broken in 2015, by this player—who still holds the record today",
+    answer: "Klay Thompson",
+    options: [
+      "Kevin Love",
+      "Devin Booker",
+      "Klay Thompson",
+      "James Harden"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The first NBA game was played between the New York Knicks and the Toronto Huskies in what year?",
+    answer: "1946",
+    options: [
+      "1946",
+      "1947",
+      "1960",
+      "1990"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The National Basketball Association was originally called what?",
+    answer: "The Basketball Association of America",
+    options: [
+      "The National NBA",
+      "The National Basketball League",
+      "The National Basketball Association",
+      "The Basketball Association of America"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Held in Boston Madison Square Garden Club, the first NBA All-Star game took place in what year?",
+    answer: "1951",
+    options: [
+      "1960",
+      "1950",
+      "1951",
+      "1975"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which NBA team has made the most appearances in the finals?",
+    answer: "Los Angeles Lakers",
+    options: [
+      "Boston Celtics",
+      "Los Angeles Lakers",
+      "Dallas Mavericks",
+      "Detroit Pistons"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The Lakers are tied with what other NBA team with 17 Championships?",
+    answer: "Boston Celtics",
+    options: [
+      "Golden State Warriors",
+      "Chicago Bulls",
+      "Boston Celtics",
+      "New York Knicks"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In 1995, this 6-seed was the lowest-seeded team in NBA history to win a Championship",
+    answer: "Houston Rockets",
+    options: [
+      "Chicago Bulls",
+      "Boston Celtics",
+      "Los Angeles Lakers",
+      "Houston Rockets"
+    ]
+  },
+  {
+    numb: 8,
+    question: "The first and only team based outside the United States to win an NBA title or reach the NBA Finals",
+    answer: "Toronto Raptors",
+    options: [
+      "Milwaukee Bucks",
+      "Denver Nuggets",
+      "Toronto Raptors",
+      "Cleveland Cavaliers"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which player has the most career blocks in the Finals?",
+    answer: "Kareem Abdul-Jabbar",
+    options: [
+      "Shaquille O'Neal",
+      "Tim Duncan",
+      "Kareem Abdul-Jabbar",
+      "Hakeem Olajuwon"
+    ]
+  },
+  {
+    numb: 10,
+    question: "This player has the most turnovers in the Finals",
+    answer: "LeBron James",
+    options: [
+      "LeBron James",
+      "Magic Johnson",
+      "Kobe Bryant",
+      "Kareem Abdul-Jabbar"
+    ]
+  },
+  {
+    numb: 11,
+    question: "In one series, who had the most steals?",
+    answer: "Isaiah Thomas",
+    options: [
+      "Chris Paul",
+      "Alvin Robertson",
+      "Isaiah Thomas",
+      "Michael Ray Richardson"
+    ]
+  },
+  {
+    numb: 12,
+    question: "This player had the most 3-point field goals, during one series of the Finals",
+    answer: "Steph Curry",
+    options: [
+      "Klay Thompson",
+      "Steph Curry",
+      "LeBron James",
+      "James Harden"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Who is the player that led in overall career point tally in the Finals?",
+    answer: "Jerry West",
+    options: [
+      "Kareem Abdul-Jabbar",
+      "Michael Jordan",
+      "LeBron James",
+      "Jerry West"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What NBA team has made the most franchise Final appearances without winning a title?",
+    answer: "Phoenix Suns",
+    options: [
+      "Miami Heat",
+      "Houston Rockets",
+      "Phoenix Suns",
+      "Indiana Pacers"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which player managed to win the NBA Finals MVP Award, even though they were on the losing team?",
+    answer: "Jerry West",
+    options: [
+      "Kevin Durant",
+      "Wilt Chamberlain",
+      "Magic Johnson",
+      "Jerry West"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which was the first team to make it to the NBA finals ten years running?",
+    answer: "Boston Celtics",
+    options: [
+      "Chicago Bulls",
+      "Boston Celtics",
+      "Los Angeles Lakers",
+      "Washington Wizards"
+    ]
+  },
+  {
+    numb: 17,
+    question: "A legendary player, he had the most NBA Finals appearances",
+    answer: "Bill Russell",
+    options: [
+      "Wilt Chamberlain",
+      "Michael Jordan",
+      "Bill Russell",
+      "Kareem Abdul-Jabbar"
+    ]
+  },
+  {
+    numb: 18,
+    question: "The last team to sweep their opponent in a Final",
+    answer: "Golden State Warriors",
+    options: [
+      "Detroit Pistons",
+      "Cleveland Cavaliers",
+      "Golden State Warriors",
+      "San Antonio Spurs"
+    ]
+  },
+  {
+    numb: 19,
+    question: "This team has the best winning percentage in the Finals",
+    answer: "Chicago Bulls",
+    options: [
+      "Miami Heat",
+      "Los Angeles Lakers",
+      "Boston Celtics",
+      "Chicago Bulls"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which team won the first NBA Championship?",
+    answer: "Philadelphia Warriors",
+    options: [
+      "Anderson Packers",
+      "Philadelphia Warriors",
+      "Indianapolis Jets",
+      "Waterloo Hawks"
+    ]
+  },
+  {
+    numb: 21,
+    question: "The first team to hire a woman on their coaching staff?",
+    answer: "San Antonio Spurs",
+    options: [
+      "Dallas Mavericks",
+      "Brooklyn Nets",
+      "Atlanta Hawks",
+      "San Antonio Spurs"
+    ]
+  },
+  {
+    numb: 22,
+    question: "In 2004 an altercation occurred mid-game between two teams in an incident that became known as ‘Malice at the Palace’. Which two teams were involved?",
+    answer: "Indiana Pacers & Detroit Pistons",
+    options: [
+      "Indiana Pacers & Detroit Pistons",
+      "New York Knicks & Boston Celtics",
+      "Los Angeles Lakers & Miami Heat",
+      "Golden State Warriors & Phoenix Suns"
+    ]
+  },
+  {
+    numb: 23,
+    question: "How many championships did the Boston Celtics win from 1957 to 1969?",
+    answer: "11",
+    options: [
+      "10",
+      "13",
+      "11",
+      "9"
+    ]
+  },
+  {
+    numb: 24,
+    question: "This Miami team joined the NBA in 1988",
+    answer: "Miami Heat",
+    options: [
+      "Orlando Magic",
+      "Miami Heat",
+      "Indiana Pacers",
+      "Memphia Grizzlies"
+    ]
+  },
+  {
+    numb: 25,
+    question: "Which team did Larry Bird coach during the 1997-98 season?",
+    answer: "Indian Pacers",
+    options: [
+      "Oklohoma City Thunder",
+      "Brooklyn Nets",
+      "New York Knicks",
+      "Indian Pacers"
+    ]
+  },
+  {
+    numb: 26,
+    question: "Who was the shortest player to ever win the Slam Dunk contest?",
+    answer: "Spud Webb",
+    options: [
+      "Earl Boykins",
+      "Muggsy Bogues",
+      "Spud Webb",
+      "Keith Jennings"
+    ]
+  },
+  {
+    numb: 27,
+    question: "Which player attempted a record number of free throws (39) in an NBA post-season game?",
+    answer: "Shaquille O’Neal",
+    options: [
+      "Bob Cousy",
+      "Shaquille O’Neal",
+      "DeAndre Jordan",
+      "Michael Jordan"
+    ]
+  },
+  {
+    numb: 28,
+    question: "Who was the first player to win the MVP in consecutive seasons?",
+    answer: "Bill Russell",
+    options: [
+      "Bill Russell",
+      "Wilt Chamberlain",
+      "Kareem Abdul-Jabbar",
+      "Michael Jordan"
+    ]
+  },
+  {
+    numb: 29,
+    question: "Who was the first player in the NBA to hit an elusive triple-double season?",
+    answer: "Oscar Robertson",
+    options: [
+      "Nikola Jokic",
+      "Russell Westbrook",
+      "LeBron James",
+      "Oscar Robertson"
+    ]
+  },
+  {
+    numb: 30,
+    question: "The first NBA athlete to win both the MVP and Rookie of the Year titles in the same season?",
+    answer: "Wilt Chamberlain",
+    options: [
+      "Wilt Chamberlain",
+      "Derrick Rose",
+      "Magic Johnson",
+      "Stephen Curry"
+    ]
+  },
+  {
+    numb: 31,
+    question: "Who owns the Los Angeles Lakers?",
+    answer: "Jeanie Buss",
+    options: [
+      "Joe Lacob",
+      "Tilman Fertitta",
+      "Jeanie Buss",
+      "Herbert Simon"
+    ]
+  },
+  {
+    numb: 32,
+    question: "How many personal fouls does it take to get ejected from an NBA basketball game?",
+    answer: "6",
+    options: [
+      "5",
+      "6",
+      "3",
+      "1"
+    ]
+  },
+  {
+    numb: 33,
+    question: "When was the shot clock introduced into NBA regulation?",
+    answer: "1955",
+    options: [
+      "1990",
+      "1950",
+      "1962",
+      "1955"
+    ]
+  },
+  {
+    numb: 34,
+    question: "How tall is the hoop in the NBA?",
+    answer: "10 feet",
+    options: [
+      "9 feet",
+      "12 feet",
+      "10 feet",
+      "13 feet"
+    ]
+  },
+  {
+    numb: 35,
+    question: "When a team is granted a time-out, play will resume only when...",
+    answer: "The time-out clock has expired",
+    options: [
+      "The teams make free throws",
+      "The coaches fight",
+      "The game is over",
+      "The time-out clock has expired"
+    ]
+  },
+  {
+    numb: 36,
+    question: "Who invented the sport of basketball?",
+    answer: "James Naismith",
+    options: [
+      "James Naismith",
+      "Jerry West",
+      "Walter Meanwell",
+      "Serge Ibaka"
+    ]
+  },
+  {
+    numb: 37,
+    question: "When was basketball first introduced into the Olympics?",
+    answer: "1936",
+    options: [
+      "1940",
+      "1936",
+      "1950",
+      "1935"
+    ]
+  },
+  {
+    numb: 38,
+    question: "How many teams play in the NBA?",
+    answer: "30",
+    options: [
+      "30", 
+      "27",
+      "20",
+      "34"
+    ]
+  },
+  {
+    numb: 39,
+    question: "Who was the 2001 number 1 overall pick in the draft?",
+    answer: "Kwame Brown",
+    options: [
+      "Tyson Chandler", 
+      "Pau Gasol",
+      "Joe Johnson",
+      "Kwame Brown"
+    ]
+  },
+  {
+    numb: 40,
+    question: "Who won the NBA dunk contest in 2016",
+    answer: "Zach Lavine",
+    options: [
+      "Aaron Gordon", 
+      "Zach Lavine",
+      "Victor Oladipo",
+      "Will Barton"
+    ]
+  },
+  {
+    numb: 41,
+    question: "Magic Johnson was inducted into the NBA Hall of Fame in what year?",
+    answer: "2002",
+    options: [
+      "1999", 
+      "2000",
+      "2001",
+      "2002"
+    ]
+  },
+  {
+    numb: 42,
+    question: "How many NBA officials are in a game? ",
+    answer: "3",
+    options: [
+      "3", 
+      "2",
+      "4",
+      "5"
+    ]
+  },
+  {
+    numb: 43,
+    question: "Which NBA team won eight consecutive championships from 1959 to 1966?",
+    answer: "Boston Celtics",
+    options: [
+      "Boston Celtics", 
+      "Los Angeles Lakers",
+      "Chicago Bulls",
+      "Miami Heat"
+    ]
+  },
+  {
+    numb: 44,
+    question: "Who is the Chicago Bulls mascot?",
+    answer: "Benny the Bull",
+    options: [
+      "Lenny the Bull", 
+      "Kenny the Bull",
+      "Benny the Bull",
+      "Penny the Bull"
+    ]
+  },
+  {
+    numb: 45,
+    question: "What number did Michael Jordan wear when playing for the Chicago Bulls? ",
+    answer: "23",
+    options: [
+      "43", 
+      "30",
+      "21",
+      "23"
+    ]
+  },
+  {
+    numb: 46,
+    question: "Which famous player is the NBA logo modelled after? ",
+    answer: "Jerry West",
+    options: [
+      "Larry Bird", 
+      "Steve Nash",
+      "Jerry West",
+      "Dirk Nowitzki"
+    ]
+  },
+  {
+    numb: 47,
+    question: "Which team drafted Giannis Antetokounmpo in 2013?",
+    answer: "Milwaukee Bucks",
+    options: [
+      "Indiana Pacers", 
+      "Milwaukee Bucks",
+      "Oklohoma City Thunder",
+      "Cleveland Cavaliers"
+    ]
+  },
+  {
+    numb: 48,
+    question: "Who won the first official NBA game when it started?",
+    answer: "New York Knicks",
+    options: [
+      "Portland Trailblazers", 
+      "Brooklyn Nets",
+      "Los Angeles Lakers",
+      "New York Knicks"
+    ]
+  },
+  {
+    numb: 49,
+    question: "Who is the current NBA commissioner? ",
+    answer: "Adam Silver",
+    options: [
+      "Larry O'Brien", 
+      "Maurice Podoloff",
+      "Adam Silver",
+      "David Stern"
+    ]
+  },
+  {
+    numb: 50,
+    question: "Micheal Jordan famously retired from basketball in 1993. What sport did he go on to play instead? ",
+    answer: "Baseball",
+    options: [
+      "Baseball", 
+      "Tennis",
+      "Cricket",
+      "Golf"
+    ]
+  },
+    
+];
+
