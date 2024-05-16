dd8cc91073623bb0b3d073bd7c1b6dada4df1a18
KingAJ19 <anelesokwane@gmail.com>
2024-05-16 08:29:10 +0200

    oi

---
+++ Quizzes/Trivia/questions.js
@@ -0,0 +1,554 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who scored with his hand in the 1986 World Cup quarterfinals against England?",
+    answer: "Diego Maradona",
+    options: [
+      "Zinedine Zidane",
+      "Pele",
+      "Diego Maradona",
+      "Ronaldo"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Ex-West Ham striker Paulo Di Canio was from which country?",
+    answer: "Italy",
+    options: [
+      "Italy",
+      "Germany",
+      "Sweden",
+      "Spain"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Complete the name of this former Premier League team. Charlton _____",
+    answer: "Athletic",
+    options: [
+      "Wycombe",
+      "Barnsley",
+      "Wigan",
+      "Athletic"
+    ]
+  },
+    {
+    numb: 4,
+    question: "John Terry spent his entire playing career at which club?",
+    answer: "Chelsea",
+    options: [
+      "Manchester City",
+      "Juventus",
+      "Chelsea",
+      "Liverpool"
+    ]
+  },
+  {
+    numb: 5,
+    question: "David Seaman represented which country internationally?",
+    answer: "England",
+    options: [
+      "Germany",
+      "England",
+      "Hungary",
+      "France"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Ex-Arsenal player Dennis Bergkamp is from which country?",
+    answer: "Netherlands",
+    options: [
+      "England",
+      "France",
+      "Netherlands",
+      "Italy"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Manchester United are also known as 'The Red .......'?",
+    answer: "Devils",
+    options: [
+      "Bulls",
+      "Men",
+      "Kite",
+      "Devils"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In what position does Harry Kane play?",
+    answer: "Striker",
+    options: [
+      "Midfielder",
+      "Defender",
+      "Striker",
+      "Goalkeeper"
+    ]
+  },
+  {
+    numb: 9,
+    question: "How many teams are promoted to the Premier League each season from the division below?",
+    answer: "3",
+    options: [
+      "2",
+      "4",
+      "3",
+      "5"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Gareth Bale plays for which country?",
+    answer: "Wales",
+    options: [
+      "Wales",
+      "Sweden",
+      "Finland",
+      "Scotland"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Complete the name of this former Premier League team. Bolton _____",
+    answer: "Wanderers",
+    options: [
+      "County",
+      "Vale",
+      "Wanderers",
+      "City"
+    ]
+  },
+  {
+    numb: 12,
+    question: "David Beckham left Manchester United in 2003 to join which Spanish team?",
+    answer: "Real Madrid",
+    options: [
+      "Atletico Madrid",
+      "Real Madrid",
+      "Valencia",
+      "Getafe"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Former Arsenal manager Arsene Wenger is from which country?",
+    answer: "France",
+    options: [
+      "Italy",
+      "Portugal",
+      "Germany",
+      "France"
+    ]
+  },
+  {
+    numb: 14,
+    question: "David Beckham played for which MLS team in the United States?",
+    answer: "LA Galaxy",
+    options: [
+      "Atalanta United",
+      "Inter Miami",
+      "LA Galaxy",
+      "Orlando City"
+    ]
+  },
+  {
+    numb: 15,
+    question: "The city of Liverpool has two clubs in the Premier League - Liverpool & which other?",
+    answer: "Everton",
+    options: [
+      "West Ham",
+      "Crystal Palace",
+      "Sheffield United",
+      "Everton"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Steven Gerrard & Wayne Rooney are both from which English city?",
+    answer: "Liverpool",
+    options: [
+      "Manchester",
+      "Liverpool",
+      "Swansea",
+      "Bristol"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Fulham Football Club are situated in which English city?",
+    answer: "London",
+    options: [
+      "Birmingham",
+      "Leicester",
+      "London",
+      "Coventry"
+    ]
+  },
+  {
+    numb: 18,
+    question: "As of 2020, which of these teams have been relegated the most times from the Premier League?",
+    answer: "Norwich City",
+    options: [
+      "Sunderland",
+      "West Brom",
+      "Norwich City",
+      "Hull City"
+    ]
+  },
+  {
+    numb: 19,
+    question: "What type of weaponry features on the Arsenal club badge?",
+    answer: "Cannon",
+    options: [
+      "Machine Gun",
+      "Dagger",
+      "Shotgun",
+      "Cannon"
+    ]
+  },
+  {
+    numb: 20,
+    question: "What is the nickname of Arsenal Football Club?",
+    answer: "The Gunners",
+    options: [
+      "The Gooners",
+      "The Gunners",
+      "The Guns",
+      "The Gunner Boys"
+    ]
+  },
+  {
+    numb: 21,
+    question: "Which of these is a famous stand at Liverpool's Anfield stadium?",
+    answer: "The Kop",
+    options: [
+      "The Kopp",
+      "The Cop",
+      "The Kops",
+      "The Kop"
+    ]
+  },
+  {
+    numb: 22,
+    question: "Which Merseyside club play at Goodison Park?",
+    answer: "Everton",
+    options: [
+      "Everton",
+      "Tranmere",
+      "Southport",
+      "Pilkington"
+    ]
+  },
+  {
+    numb: 23,
+    question: "What did Eric Cantona do in 1995 that earned him a 9 month ban from football?",
+    answer: "Kung-fu kicked an opposing fan during a game",
+    options: [
+      "Took off his clothes during the game",
+      "Punched the referee in the face",
+      "Kung-fu kicked an opposing fan during a game",
+      "Threw things into the crowd"
+    ]
+  },
+  {
+    numb: 24,
+    question: "What type of animal features on the Chelsea club badge?",
+    answer: "Lion",
+    options: [
+      "Tiger",
+      "Lion",
+      "Jaguar",
+      "Panther"
+    ]
+  },
+  {
+    numb: 25,
+    question: "Which team did Thiago Alcantara sign for in the summer of 2020?",
+    answer: "Liverpool",
+    options: [
+      "Bayern Munich",
+      "Barcelona",
+      "Atletico Madrid",
+      "Liverpool"
+    ]
+  },
+  {
+    numb: 26,
+    question: "Who is the leading goal scorer in La Liga history?",
+    answer: "Lionel Messi",
+    options: [
+      "Telmo Zarra",
+      "Cristiano Ronaldo",
+      "Lionel Messi",
+      "Hugo Sanchez"
+    ]
+  },
+  {
+    numb: 27,
+    question: "Who won La Liga Best Player award 2015/2016?",
+    answer: "Antonio Griezmann",
+    options: [
+      "Lionel Messi",
+      "Antonio Griezmann",
+      "Cristiano Ronaldo",
+      "Luis Suarez"
+    ]
+  },
+  {
+    numb: 28,
+    question: "Which club has the highest number of trophies in spain?",
+    answer: "Real Madrid",
+    options: [
+      "Real Madrid",
+      "Barcelona",
+      "Atletico Madrid",
+      "Espanyol"
+    ]
+  },
+  {
+    numb: 29,
+    question: "Messi scored his first ever hat trick against which spanish club?",
+    answer: "Real Madrid",
+    options: [
+      "Real Betis",
+      "Getafe",
+      "Athletic Bilbao",
+      "Real Madrid"
+    ]
+  },
+  {
+    numb: 30,
+    question: "2010 Fifa World Cup best player named Diego Forlan who played which spanish clubs?",
+    answer: "Villareal and Atletico Madrid",
+    options: [
+      "Villareal and Atletico Madrid",
+      "Villareal and Sevilla",
+      "Real Madrid and Real Sociedad",
+      "Atletico Madrid and Real Betis"
+    ]
+  },
+  {
+    numb: 31,
+    question: "Which year did Messi scored his first ever hat trick against Real Madrid?",
+    answer: "2007",
+    options: [
+      "2009",
+      "2008",
+      "2007",
+      "2010"
+    ]
+  },
+  {
+    numb: 32,
+    question: "Which is the oldest club in spain?",
+    answer: "Sevilla",
+    options: [
+      "Real Madrid",
+      "Sevilla",
+      "Barcelona",
+      "Athletic Club"
+    ]
+  },
+  {
+    numb: 33,
+    question: "Who has won the most number of 'Zamora Trophy' by keeping highest amount of clean sheets in La Liga history?",
+    answer: "Jan Oblak",
+    options: [
+      "Iker Casillas",
+      "Victor Valdes",
+      "Mark-Andre ter Stegen",
+      "Jan Oblak"
+    ]
+  },
+  {
+    numb: 34,
+    question: "Which stadium in spain has the highest seating capacity with 99,354 seats?",
+    answer: "Camp Nou",
+    options: [
+      "Estadio Vicente Calderon",
+      "Estadio Santiago Bernabeu",
+      "Camp Nou",
+      "RCDE Stadium"
+    ]
+  },
+  {
+    numb: 35,
+    question: "Who was the first Real Madrid's manager?",
+    answer: "Arthur Johnson",
+    options: [
+      "Juande Ramos",
+      "Vicente del Bosque",
+      "Fabio Capello",
+      "Arthur Johnson"
+    ]
+  },
+  {
+    numb: 36,
+    question: "AC Milan was founded in December 1899. Which English city were the club's founders from?",
+    answer: "Nottingham",
+    options: [
+      "Nottingham",
+      "Birmingham",
+      "Bergamo",
+      "Venice"
+    ]
+  },
+  {
+    numb: 37,
+    question: "Which club, famous for their purple shirts, were the first-ever winners of the European Cup Winners' Cup in 1961?",
+    answer: "Fiorentina",
+    options: [
+      "Juventus",
+      "Fiorentina",
+      "Roma",
+      "Napoli"
+    ]
+  },
+  {
+    numb: 38,
+    question: "In which year did AC Milan win their first Campionato?",
+    answer: "1901",
+    options: [
+      "1901", 
+      "1900",
+      "1902",
+      "1905"
+    ]
+  },
+  {
+    numb: 39,
+    question: "Who was AC Milan's captain in the 2003-04 season?",
+    answer: "Paolo Maldini",
+    options: [
+      "Roberto Antonelli", 
+      "Frank Baresi",
+      "Caesar Maldini",
+      "Paolo Maldini"
+    ]
+  },
+  {
+    numb: 40,
+    question: "Which striker was topscorer three times in the 90s, when he played for Lazio?",
+    answer: "Giuseppe Signori",
+    options: [
+      "Gabriel Batistuta", 
+      "Giuseppe Signori",
+      "Roberto Baggio",
+      "Ciro Immobile"
+    ]
+  },
+  {
+    numb: 41,
+    question: "Against which Spanish side did Juventus win the UEFA Cup in 1977?",
+    answer: "Athletico Bilbao",
+    options: [
+      "Osasuna", 
+      "Sevilla",
+      "Atletico Madrid",
+      "Athletico Bilbao"
+    ]
+  },
+  {
+    numb: 42,
+    question: "The first FIFA World Cup tournament was held in",
+    answer: "1930",
+    options: [
+      "1930", 
+      "1929",
+      "1940",
+      "1928"
+    ]
+  },
+  {
+    numb: 43,
+    question: "Which country became the first from Africa to compete at a World Cup finals?",
+    answer: "Egypt",
+    options: [
+      "Egypt", 
+      "Morocco",
+      "Tunisia",
+      "Algeria"
+    ]
+  },
+  {
+    numb: 44,
+    question: "Which country was the first to win two World Cups?",
+    answer: "Italy",
+    options: [
+      "Brazil", 
+      "Scotland",
+      "Italy",
+      "Germany"
+    ]
+  },
+  {
+    numb: 45,
+    question: "No country outside of Europe or South America has ever won the men’s World Cup. True or False?",
+    answer: "True",
+    options: [
+      "Neither", 
+      "Both",
+      "False",
+      "True"
+    ]
+  },
+  {
+    numb: 46,
+    question: "What was bizarre about Australia’s qualification for the 1998 World Cup?",
+    answer: "They were unbeaten but still didn’t qualify for the tournament",
+    options: [
+      "They competed with the CONMEBOL nations for a place", 
+      "They had four different managers",
+      "They were unbeaten but still didn’t qualify for the tournament",
+      "None of their starting XI against Fiji was born in Australia"
+    ]
+  },
+  {
+    numb: 47,
+    question: "Who won the top scorer title at the tournament on Mexican soil in 1986?",
+    answer: "Gary Lineker",
+    options: [
+      "Diego Maradona", 
+      "Gary Lineker",
+      "Michel Platini",
+      "Zico"
+    ]
+  },
+  {
+    numb: 48,
+    question: "This is the first tournament for both Lionel Messi and Cristiano Ronaldo. How many goals did they score each (2006)?",
+    answer: "1",
+    options: [
+      "4", 
+      "6",
+      "4",
+      "1"
+    ]
+  },
+  {
+    numb: 49,
+    question: "Who won the Best Young Player award in 2014?",
+    answer: "Paul Pogba",
+    options: [
+      "Memphis Depay", 
+      "James Rodriguez",
+      "Paul Pogba",
+      "Luke Shaw"
+    ]
+  },
+  {
+    numb: 50,
+    question: "The 2018 tournament is a record-setting tournament for the number of-",
+    answer: "Most own goals",
+    options: [
+      "Most own goals", 
+      "Most goals",
+      "Most hat-tricks",
+      "Most red cards"
+    ]
+  },
+    
+];
+
